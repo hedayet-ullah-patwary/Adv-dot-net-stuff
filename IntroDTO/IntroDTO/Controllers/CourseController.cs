6da3d5d215b13475087784277beebb0b76637fab
hedayet-ullah-patwary <hedayetullahpatwary@gmail.com>
2026-04-17 23:10:40 +0600

    create CourseDTO

---
+++ IntroDTO/IntroDTO/Controllers/CourseController.cs
@@ -0,0 +1,50 @@
+﻿using IntroDTO.DTOs;
+using IntroDTO.EF;
+using IntroDTO.EF.Tables;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntroDTO.Controllers
+{
+    public class CourseController : Controller
+    {
+        StudentMsASp26Context db;
+
+        public CourseController(StudentMsASp26Context db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //without DTO
+            var depts = db.Departments.ToList();
+            ViewBag.Departments = depts;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CourseDTO c)
+        {
+            if (ModelState.IsValid)
+            {
+                var st = new Course()
+                {
+                    Name = c.Name,
+                    Did = c.Did
+                };
+                db.Courses.Add(st); 
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            var depts = db.Departments.ToList();
+            ViewBag.Departments = depts;
+            return View(c); 
+        }
+    }
+}
---
+++ IntroDTO/IntroDTO/DTOs/CourseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntroDTO.DTOs
+{
+    public class CourseDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public int Did { get; set; }
+    }
+}
